95d3c4b21f484cb992ee410b4e31e41aad6f5875
protectflame <magaomaev@gmail.com>
2025-12-10 13:03:07 +0300

    methods main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    static void main() {
+    static void main(String[] args) {
+        Student st1 = new Student("Омаев Магомед", 18, new ArrayList<String>());
+        Student st2 = new Student("Насрулаева Айзар", 17, new ArrayList<String>());
+        Student st3 = new Student("Алиев Шамиль", 17, new ArrayList<String>());
 
     }
 }
